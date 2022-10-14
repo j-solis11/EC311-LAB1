8ed66093ea0dd2e8252c26eb5dcc010297a99015
j-solis11 <114105274+j-solis11@users.noreply.github.com>
2022-10-14 19:43:40 -0400

    initial commit

--- add_sub_4_bit/hdl/add_sub_4_bit.v
+++ add_sub_4_bit/hdl/add_sub_4_bit.v
@@ -1,25 +1,44 @@
 `timescale 1ns / 1ps
-
-// EC-311 Lab-1 Part-1
-  // The names of the variables are as described in the lab handout
-
-module add_sub_4_bit #
-(
-  parameter WIDTH = 4
-)
-(
-  // The inputs 
-  input wire [WIDTH-1:0]        A_i,
-  input wire [WIDTH-1:0]        B_i,
-  input wire                    M_i,
-
-  // The outputs
-  output wire                   V_o,
-  output wire                   C_o,
-  output wire [WIDTH-1:0]       S_o
-
-);
-
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/30/2022 01:42:12 PM
+// Design Name: 
+// Module Name: 4bitadder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
 
+module add_sub_4_bit.v(
+    input M,
+    input [3:0]A_in,
+    input [3:0]B_in,
+    output [3:0]S_out,
+    output V,
+    output C4
+    );
+    wire [3:0]w0;
+    wire c3;
+    xor G0(w0[0],B_in[0],M);
+    xor G1(w0[1],B_in[1],M);
+    xor G2(w0[2],B_in[2],M);
+    xor G3(w0[3],B_in[3],M);
+    fbadder bita(.A_i(A_in), 
+                 .B_i(w0), 
+                 .c_i(M), 
+                 .s_o(S_out), 
+                 .c_o3(c3),
+                 .c_o4(C4));
+    xor G4(V,c3,C4);           
 endmodule
