326eceae256dd5d1dc3f4eadcad7736fd23cab59
j-solis11 <114105274+j-solis11@users.noreply.github.com>
2022-10-14 20:07:57 -0400

    Update add_sub_4_bit.v

--- add_sub_4_bit/hdl/add_sub_4_bit.v
+++ add_sub_4_bit/hdl/add_sub_4_bit.v
@@ -19,26 +19,33 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
+module add_sub_4_bit # 
+(
+  parameter WIDTH = 4
+)
+(
+  // The inputs 
+  input wire [WIDTH-1:0]        A_i,
+  input wire [WIDTH-1:0]        B_i,
+  input wire                    M_i,
 
-module add_sub_4_bit.v(
-    input M,
-    input [3:0]A_in,
-    input [3:0]B_in,
-    output [3:0]S_out,
-    output V,
-    output C4
-    );
+  // The outputs
+  output wire                   V_o,
+  output wire                   C_o,
+  output wire [WIDTH-1:0]       S_o
+
+);
     wire [3:0]w0;
     wire c3;
-    xor G0(w0[0],B_in[0],M);
-    xor G1(w0[1],B_in[1],M);
-    xor G2(w0[2],B_in[2],M);
-    xor G3(w0[3],B_in[3],M);
-    fbadder bita(.A_i(A_in), 
+    xor G0(w0[0],B_i[0],M_i);
+    xor G1(w0[1],B_i[1],M_i);
+    xor G2(w0[2],B_i[2],M_i);
+    xor G3(w0[3],B_i[3],M_i);
+    fbadder bita(.A_i(A_i), 
                  .B_i(w0), 
-                 .c_i(M), 
-                 .s_o(S_out), 
+                 .c_i(M_i), 
+                 .s_o(S_o), 
                  .c_o3(c3),
-                 .c_o4(C4));
-    xor G4(V,c3,C4);           
+                 .c_o4(C_o));
+    xor G4(V_o,c3,C_o);           
 endmodule
