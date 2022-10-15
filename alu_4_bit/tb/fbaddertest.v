780ab43619435aa6e8b7e47cefca49a9c656b5a2
j-solis11 <114105274+j-solis11@users.noreply.github.com>
2022-10-14 20:02:52 -0400

    Create fbaddertest.v

---
+++ alu_4_bit/tb/fbaddertest.v
@@ -0,0 +1,38 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/12/2022 10:54:04 PM
+// Design Name: 
+// Module Name: addertest
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module fbaddertest(
+
+    );
+    reg [3:0] A_i, B_i;
+    wire [7:0] Y_o;
+    
+    fbadder fba0(.A_i(A_i),.B_i(B_i),.Y_o(Y_o));
+    
+    initial begin
+        A_i = 0; B_i = 0;
+    end
+    always begin
+        #2 {A_i,B_i} = {A_i,B_i} + 1'b1;
+        if ((A_i == 4'b1111) && (B_i == 4'b1111)) #10 $finish; 
+    end   
+endmodule
